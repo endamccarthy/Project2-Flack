871a2bb6026c216aa73e68b1ad4710152c4789c8
Enda McCarthy <enda.mccarthy@outlook.com>
2019-05-30 16:02:39 +0100

    test

---
+++ core/__init__.py
@@ -0,0 +1,20 @@
+from flask import Flask
+from flask_socketio import SocketIO, emit
+from core.config import Config
+
+
+socketio = SocketIO()
+
+def create_app(config_class=Config):
+    # start up flask
+    app = Flask(__name__)
+    app.config.from_object(Config)
+
+    # initialize application features
+    socketio.init_app(app)
+
+    # blueprints setup
+    from core.main.routes import main
+    app.register_blueprint(main)
+
+    return app
