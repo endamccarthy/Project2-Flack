c622fb95f84c759dc7465ff230cba36c8a2807e5
Enda McCarthy <enda.mccarthy@outlook.com>
2019-07-17 17:49:14 +0100

    main work done, fine tuning

--- application.py
+++ application.py
@@ -1,9 +1,7 @@
-import os
-import time
+import os, time
 
-from flask import Flask, jsonify, render_template, request
-from flask_socketio import SocketIO, emit
-
+from flask import Flask, render_template, request
+from flask_socketio import emit, SocketIO
 from collections import deque
 
 app = Flask(__name__)
@@ -16,24 +14,23 @@ socketio = SocketIO(app)
 USERS = {}
 CHANNELS = {"Public": deque([], maxlen=100)}
 
-'''
-channels = {"total": 0}
-channelNames = []
-'''
 
 @app.route("/")
 def index():
     return render_template("index1.html")
 
+# DELETE.....
 @socketio.on('connect')
 def connection():
     print("new user connected")
+
 
 # if a username is passed in then the associated session ID is assigned to the username
 @socketio.on('userdata')
 def user_data(data):
     if 'username' in data:
         USERS[data['username']] = request.sid
+
 
 # checks if a channel name isn't taken and saves it to the CHANNELS dictionary (only 100 most recent messages in channel will be saved)
 @socketio.on('new channel')
@@ -44,6 +41,7 @@ def new_channel(data):
         CHANNELS[data['name']] = deque(maxlen=100)
         emit('new channel', {"name": data['name']}, broadcast=True)
 
+
 # attaches timestamp to new message, saves message to selected channel
 @socketio.on('new message')
 def new_message(data):
@@ -52,44 +50,27 @@ def new_message(data):
         CHANNELS[data['channel']].append(data)
         emit('message', data, broadcast=True)
 
+
 # emits a list of all the channel names
 @socketio.on('get channels')
 def get_channels():
     emit('channels', list(CHANNELS.keys()))
 
+
 # emits a list of all the messages in a selected channel
-@socketio.on('get messgaes')
+@socketio.on('get messages')
 def get_messages(data):
     if 'name' in data:
         emit('messages', list(CHANNELS[data['name']]))
 
-'''
-@app.route("/signin", methods=["POST"])
-def signin():
-    # query for username
-    username = request.form.get("username")
-    # make sure username is valid
-    if len(username) < 1:
-        return jsonify({"success": False})
-    return jsonify({"success": True})
 
+# DELETE....
+@socketio.on('reset')
+def reset():
+    USERS.clear()
+    CHANNELS.clear()
+    CHANNELS['Public'] = deque(maxlen=100)
 
-@socketio.on("channelNameCheck")
-def channelNameCheck(channelName):
-    if channelName in channelNames:
-        emit("addChannel", {"channelName": "invalid"}, broadcast=False)
-        return False
-    channelNames.append(channelName)
-    channels["total"] += 1
-    emit("addChannel", {"channelName": channelName}, broadcast=True)
-
-
-@socketio.on("addMessage")
-def addMessage(data):
-    message = data["message"]
-    emit("newMessage", {"message": message}, broadcast=True)
-'''
 
 if __name__ == '__main__':
     socketio.run(app)
-    
--- static/index1.js
+++ static/index1.js
@@ -2,7 +2,40 @@ document.addEventListener("DOMContentLoaded", () => {
     get_username();
 })
 
+
 // here we are creating a funcion and a constant variable and assigning the function to the variable
+const get_username = () => {
+    // retrieve username if already saved in local storage
+    let username = localStorage.getItem("username");
+
+    // if no username exists the username form modal is shown, otherwise the initialize function is called
+    if(!username) {
+        $(".modal").modal({ show: true, backdrop: "static"});
+
+        document.querySelector("#username-form").addEventListener("submit", e => {
+            e.preventDefault();
+            username = document.querySelector("#username-text").value;
+            console.log(username);
+            // if username field is empty then nothing happens, otherwise save to local storage and initialize
+            if (typeof username == "string") {
+                username = username.trim()
+                if (username == "") {
+                    username = null;
+                }
+                else {
+                    localStorage.setItem("username", username);
+                    $(".modal").modal("hide");
+                    initialize(username);
+                }
+            }
+        });
+    }
+    else {
+        initialize(username);
+    }
+};
+
+
 const initialize = username => {
     // connect to web socket
     let socket = io.connect(location.protocol + "//" + document.domain + ":" + location.port);
@@ -11,6 +44,16 @@ const initialize = username => {
     socket.on("connect", () => {
         socket.emit("userdata", { username });
         setup(socket);
+
+        // NOT WORKING...FIX....
+        // sets the public channel as the default if no other channel has been selected
+        if(!localStorage.getItem("channel")) {
+            document.querySelectorAll("#channel-list > li").forEach(e => {
+                if(e.innerHTML == "Public") {
+                    e.classList.add("active");
+                }
+            });
+        };
 
         socket.on("new channel", data => {
             show_channel(data.name, socket);
@@ -33,12 +76,21 @@ const initialize = username => {
 
         socket.on("messages", data => {
             clear_messages();
-            data.forEach(message => {
-                show_message(message);
-            });
+            if(data) {
+                data.forEach(message => {
+                    show_message(message);
+                });
+            }
         });
+
+        document.querySelector('#reset').onclick = () => {
+            socket.emit("reset");
+            localStorage.removeItem("username");
+        };
+
     });
 };
+
 
 const setup = socket => {
     let channel_form = document.querySelector("#channel-form");
@@ -86,37 +138,92 @@ const setup = socket => {
 
     // if a channel has been selected then look to server side memory for list of all messages in the channel
     if(localStorage.getItem("channel")) {
-        socket.emit("get messages", { name: localStorage.getItem("channel")});
+        socket.emit("get messages", { name: localStorage.getItem("channel") });
     }
 };
 
-const get_username = () => {
-    // retrieve username if already saved in local storage
-    let username = localStorage.getItem("username");
 
-    // if no username exists the username form modal is shown, otherwise the initialize function is called
-    if(!username) {
-        $(".modal").modal({ show: true, backdrop: "static"});
+const show_channel = (name, socket) => {
+    let ul = document.querySelector("#channel-list");
+    let li = document.createElement("li");
 
-        document.querySelector("#username-form").addEventListener("submit", e => {
-            e.preventDefault();
-            username = document.querySelector("#username-text").value;
-            console.log(username);
-            // if username field is empty then nothing happens, otherwise save to local storage and initialize
-            if (typeof username == "string") {
-                username = username.trim()
-                if (username == "") {
-                    username = null;
-                }
-                else {
-                    localStorage.setItem("username", username);
-                    $(".modal").modal("hide");
-                    initialize(username);
-                }
-            }
-        });
+    // adds the class "list-group-item" to each list item
+    li.classList.add("list-group-item");
+    li.innerHTML = name;
+
+    // when a channel is clicked on....
+    li.addEventListener("click", () => {
+        localStorage.setItem("channel", name);
+        socket.emit("get messages", { name });
+        change_message_title(name);
+        show_active_channel(name);
+    });
+
+    ul.appendChild(li);
+};
+
+
+const show_message = data => {
+    if(localStorage.getItem("channel") == data.channel) {
+        let ul = document.querySelector("#message-list");
+        let li = document.createElement("li");
+        li.classList.add("list-group-item");
+
+        // shows username in bold, then the message, then the date created at in small text
+        li.innerHTML = `<strong>${data.username}</strong>: ${data.message} <small class="text-muted d-flex justify-content-end">
+                        ${get_date_string(data.created_at)}</small>`;
+        ul.appendChild(li);
+
+        // enables scrolling of messages
+        ul.scrollTop = ul.scrollHeight - ul.clientHeight;
     }
-    else {
-        initialize(username);
+};
+
+
+const show_active_channel = name => {
+    // iterates through each list item checking to see if the text equals the name selected
+    document.querySelectorAll("#channel-list > li").forEach(e => {
+        if(e.innerHTML == name) {
+            e.classList.add("active");
+        }
+        else {
+            e.classList.remove("active");
+        }
+    });
+};
+
+
+const change_message_title = title_name => {
+    if(title_name) {
+        let title = document.querySelector("#channel-label");
+        title.innerHTML = '<span class="text-muted"># </span>' + title_name;
     }
+};
+
+
+const clear_messages = () => {
+    let ul = document.querySelector("#message-list");
+    ul.innerHTML = "";
+};
+
+
+const clear_channels = () => {
+    let ul = document.querySelector("#channel-list");
+    ul.innerHTML = "";
+};
+
+
+const get_date_string = time => {
+    time = new Date(time * 1000);
+    let m_string = `${time.toDateString().split(" ")[1]} ${time.getDate()}`;
+
+    if(time.getFullYear() != new Date().getFullYear()) {
+        m_string += `, ${time.getFullYear()}`;
+    }
+
+    return `${time.toLocaleString("en-US", {
+        hour: "numeric",
+        minute: "numeric",
+        hour12: true
+    })} | ${m_string}`;
 };
